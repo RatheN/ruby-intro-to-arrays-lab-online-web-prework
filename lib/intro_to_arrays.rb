75cd9070760c6e408a9f58e2148c12fc233762c3
Nathan Rathe <nprathe10@gmail.com>
2019-05-28 23:29:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -19,5 +19,9 @@ def last_element(my_last_element)
 end
 
 def first_element_with_array_methods(first_country)
-  first_country.shift
+  first_country.first
+end
+
+def last_element_with_array_methods(last_country)
+  last_country.last
 end
